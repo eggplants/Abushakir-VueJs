981352ece0f7e6111fd82e594daa69b18307ca79
Nabute <nabute925@gmail.com>
2020-05-10 23:11:42 +0300

    UI design changed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <v-app>
-    <v-app-bar app flat absolute prominent>
-      <v-row align="center" justify="center" class="mt-10">
-        <div>
-          <span class="display-3 font-weight-bold primary--text">
-            Abush<span class="font-weight-thin">akir</span><span class="headline">Js</span>
-          </span>
-        </div>
-      </v-row>
-    </v-app-bar>
 
     <v-content>
       <HelloWorld />
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,107 @@
 <template>
   <v-container fluid>
     <v-row align="center" justify="center">
-      <v-col cols="12">
+      <!-- Today # and notifications -->
+      <v-col cols="3">
         <!-- Today -->
-        <v-row align="center" justify="center" class="my-5">
+        <!-- <v-row align="center" justify="center" class="my-5">
           <div>
             <span class="display-2">{{ today }}</span>
           </div>
-        </v-row>
+        </v-row>-->
         <!-- Weekdays -->
-        <v-row align="center" justify="center" class="mt-12">
+        <!-- <v-row align="center" justify="center" class="mt-12">
           <v-col align="center" cols="1" v-for="(days, i) in weekdays" :key="i">
             <span class="title font-weight-bold">{{ days }}</span>
           </v-col>
-        </v-row>
+        </v-row>-->
         <!-- Days -->
+        <v-row align="center" justify="center">
+          <div>
+            <span class="display-2 font-weight-bold primary--text">
+              Abush
+              <span class="font-weight-thin">akir</span>
+              <span class="headline">Js</span>
+            </span>
+          </div>
+        </v-row>
+        <v-container fill-height>
+          <v-col align="center">
+            <span style="font-size: 180px">{{etDate.dayGeez}}</span>
+            <div class="my-0 py-0">
+              <span class="display-1">{{etc.weekdays[etDate.day]}}</span>
+            </div>
+            <!-- Notification goes here -->
+          </v-col>
+        </v-container>
+      </v-col>
+      <!-- calendar grid -->
+      <!-- TODO: Try to use Tabs for the months list to be clear and nice. -->
+      <v-col cols="8" align="start">
+        <!-- This month -->
+        <div>
+          <span class="display-1 font-weight-bold">{{ toMonthGeez }}</span>
+        </div>
+        <!-- All Months -->
+        <v-btn-toggle v-model="toggle_months" mandatory>
+          <template v-for="(month, i) in etc.allMonths">
+            <v-btn :key="i" text dense style="height: 30px">
+              <span>{{ month }}</span>
+            </v-btn>
+          </template>
+        </v-btn-toggle>
+        <!-- Weekdays -->
+        <v-row align="center" justify="start" no-gutters>
+          <v-col align="start" v-for="(days, i) in etc.weekdays" :key="i">
+            <span class="title font-weight-bold">{{ days }}</span>
+          </v-col>
+        </v-row>
+      </v-col>
+      <!-- year list -->
+      <v-col cols="1">
+        <span>Years</span>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { EtDatetime, ETC } from "abushakir";
+import { etctime, ETC, EtDatetime } from "abushakir";
 export default {
   data: () => ({
-    tracked: {
-      // '2019-01-09': [23, 45, 10],
-      // '2019-01-08': [10],
-      // '2019-01-07': [0, 78, 5],
-      // '2019-01-06': [0, 0, 50],
-      // '2019-01-05': [0, 10, 23],
-      // '2019-01-04': [2, 90],
-      // '2019-01-03': [10, 32],
-      // '2019-01-02': [80, 10, 10],
-      // '2019-01-01': [20, 25, 10],
-    },
-    colors: ["#1867c0", "#fb8c00", "#000000"],
-    category: ["Development", "Meetings", "Slacking"]
+    toggle_months: this.etc.allMonths.indexof(this.toMonthGeez)
   }),
   computed: {
+    etDate() {
+      return new EtDatetime();
+    },
+    etc() {
+      return new ETC(this.etDate.year, this.etDate.month, this.etDate.day + 4);
+    },
     today() {
-      let td = new EtDatetime();
+      let td = new etctime();
       console.log("8888888888888888");
       console.log(new ETC(2012).weekdays);
-      let name = `${td.monthGeez} ${td.day < 10 ? `0${td.day}` : td.day}, ${td.year}`
+      let name = `${td.monthGeez} ${td.day < 10 ? `0${td.day}` : td.day}, ${
+        td.year
+      }`;
       return name;
     },
+    toDateGeez() {
+      return this.etc.weekdays[this.etc.day - 1];
+    },
+    toDateArabic() {
+      return this.etc.day;
+    },
+    toMonthGeez() {
+      return this.etc.allMonths[this.etc.month - 1];
+    },
+    toMonthArabic() {
+      return this.etc.month;
+    },
+    // toYear() {},
     thisYear() {
-      return new EtDatetime().year;
+      return new etctime().year;
     },
     weekdays() {
       return new ETC(this.thisYear).weekdays;
@@ -57,7 +111,8 @@ export default {
     }
   },
   created() {
-    console.log(new EtDatetime());
+    // this.etc = new ETC();
+    console.log(this.etc.allMonths.indexof(this.toMonthGeez));
   }
 };
 </script>
