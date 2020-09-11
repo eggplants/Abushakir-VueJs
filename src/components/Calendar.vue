b5096bdad1f4a4b56c4d75821f93aa227d22b075
Nabute <nabute925@gmail.com>
2020-09-12 00:52:26 +0300

    feat(DATE): ethiopic date support integrated

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,32 +3,52 @@
     <v-card-title primary-title>
       <v-row class="mx-auto" align="center">
         <v-col cols="6" align="start">
-          <span class="font-weight-regular">{{ weekdays[weekday -1] }}</span>
+          <span class="font-weight-regular">{{ weekdays[weekday] }}</span>
           <div>
             <span
               class="display-1 font-weight-thin"
-            >{{ months[monthGeez-1] }} {{ nums[date.day-1] }} ፣ {{ date.year }}</span>
+            >{{ months[monthGeez-1] }} {{ $converter(date.day) }} ፣ {{ $converter(date.year) }}</span>
           </div>
         </v-col>
         <!-- Buttons -->
         <v-col>
           <v-row justify="end" class="mx-auto">
-            <v-tooltip bottom>
+            <!-- Previous Year -->
+            <v-tooltip top>
               <template v-slot:activator="{ on }">
-                <v-btn icon fab color="white" v-on="on">
-                  <v-icon color="primary lighten-5" x-large>mdi-chevron-left</v-icon>
+                <v-btn icon color="white" v-on="on" @click="prevYear">
+                  <v-icon color="bgd lighten-5">mdi-chevron-double-left</v-icon>
+                </v-btn>
+              </template>
+              <span>{{ lastYr[idx] }}</span>
+            </v-tooltip>
+            <!-- Previous Month -->
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn icon color="white" v-on="on" class="mx-2" @click="prevMonth">
+                  <v-icon color="bgd lighten-5">mdi-chevron-left</v-icon>
                 </v-btn>
               </template>
               <span>{{ lastMonth[idx] }}</span>
             </v-tooltip>
-
-            <v-tooltip bottom>
+            <div class="mx-5"></div>
+            <!-- Previous Month -->
+            <v-tooltip top>
               <template v-slot:activator="{ on }">
-                <v-btn icon fab color="white" v-on="on">
-                  <v-icon color="primary lighten-5" x-large>mdi-chevron-right</v-icon>
+                <v-btn icon color="white" v-on="on" class="mx-2" @click="nextMonth">
+                  <v-icon color="bgd lighten-5">mdi-chevron-right</v-icon>
                 </v-btn>
               </template>
               <span>{{ nextMon[idx] }}</span>
+            </v-tooltip>
+            <!-- Next Year -->
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn icon color="white" v-on="on" @click="nextYear">
+                  <v-icon color="bgd lighten-5">mdi-chevron-double-right</v-icon>
+                </v-btn>
+              </template>
+              <span>{{ nextYr[idx] }}</span>
             </v-tooltip>
           </v-row>
         </v-col>
@@ -46,22 +66,13 @@
     <v-card-text class="pt-0">
       <v-row align="center" justify="start" class="mx-auto">
         <template v-for="i in monthStartDay">
-          <v-btn
-            disabled
-            icon
-            :key="i"
-            large
-            width="11%"
-            fab
-            outlined
-            class="ma-2"
-          >
+          <v-btn disabled icon :key="i" large width="11%" fab outlined class="ma-2">
             <v-row class="mx-auto" justify="center" align="center">
               <div>
                 <span
                   class="title grey--text"
                   color
-                >{{ (prevMonth.monthDays().length - monthStartDay) + i }}</span>
+                >{{ $converter((prevMonth.monthDays().length - monthStartDay) + i) }}</span>
               </div>
             </v-row>
           </v-btn>
@@ -74,11 +85,14 @@
             width="11%"
             fab
             class="ma-2"
-            :class="i[2] == today ? 'primary elevation-20' : 'button elevation-2'"
+            :class="i[2] == today ? 'bgd elevation-20' : 'button elevation-2'"
           >
             <v-row class="mx-auto" justify="center" align="center">
               <div>
-                <span class="title white--text">{{i[2]}}</span>
+                <span
+                  class="headline"
+                  :class="i[2] == today ? 'button--text' : 'white--text'"
+                >{{ $converter(i[2]) }}</span>
               </div>
             </v-row>
           </v-btn>
@@ -90,17 +104,20 @@
 
 <script>
 import { mapState, mapMutations } from "vuex";
-import { ETC, EtDatetime } from "abushakir";
 export default {
   data() {
     return {
       idx: 0,
       lastMonth: ["ያለፈው ወር", "Last Month"],
+      lastYr: ["ያለፈው ዓመት (አምና)", "Last Year"],
       nextMon: ["ቀጣይ ወር", "Next Month"],
+      nextYr: ["ቀጣይ ዓመት", "Next Year"],
       months: [],
       nums: [],
       weekday: "",
-      weekdays: []
+      weekdays: [],
+      dt: null,
+      etc: null,
     };
   },
   computed: {
@@ -111,8 +128,8 @@ export default {
       "today",
       "date",
       "monthGeez",
-      "monthStartDay"
-    ])
+      "monthStartDay",
+    ]),
   },
   created() {
     this.init();
@@ -124,21 +141,33 @@ export default {
       "setPrevMonth",
       "setToday",
       "setDate",
-      "setAllDays"
+      "setAllDays",
     ]),
     init() {
-      let dt = new EtDatetime();
-      let etc = new ETC(dt.year, dt.month, dt.day);
-      this.weekday = dt.weekday
-      this.setThisMonth(etc.monthDays());
-      this.setDate(dt.date);
-      this.months = etc.allMonths;
-      this.nums = etc.dayNumbers;
-      this.setAllDays(etc.monthDays.length);
-      this.setPrevMonth(etc.prevMonth);
-      this.weekdays = etc.weekdays;
-      this.setToday(dt.day);
-    }
-  }
+      this.dt = new this.$etDT();
+      this.etc = new this.$etc(this.dt.year, this.dt.month, this.dt.day);
+      this.weekday = this.dt.weekday;
+      this.setThisMonth(this.etc.monthDays());
+      this.setDate(this.dt.date);
+      this.months = this.etc.allMonths;
+      this.nums = this.etc.dayNumbers;
+      this.setAllDays(this.etc.monthDays.length);
+      this.setPrevMonth(this.etc.prevMonth);
+      this.weekdays = this.etc.weekdays;
+      this.setToday(this.dt.day);
+    },
+    nextMonth() {
+      console.log("going to next month...");
+    },
+    prevMonth() {
+      console.log("going to previous month...");
+    },
+    nextYear() {
+      console.log("going to next year...");
+    },
+    prevYear() {
+      console.log("going to previous year...");
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from "./store/index";
-// import { EtDatetime, ETC, BahireHasab } from "abusahakir";
+import { EtDatetime, ETC, BahireHasab, ConvertToEthiopic } from "abushakir";
 
 Vue.config.productionTip = false
 
-// Vue.use(EtDatetime, ETC, BahireHasab);
+
+Object.defineProperty(Vue.prototype, '$etDT', { value: EtDatetime });
+Object.defineProperty(Vue.prototype, '$etc', { value: ETC });
+Object.defineProperty(Vue.prototype, '$bh', { value: BahireHasab });
+Object.defineProperty(Vue.prototype, '$converter', { value: ConvertToEthiopic });
 
 new Vue({
   vuetify,
